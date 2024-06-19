e83fa759527ecd4bf5a49045b5c1bb757aacc4b5
IsabelaYabe <103371464+IsabelaYabe@users.noreply.github.com>
2024-06-19 10:56:36 -0300

    atualizar classe lime e sp

--- lime_functions.py
+++ lime_functions.py
@@ -40,61 +40,92 @@ class LimeExplainer:
     
 # Lime para explicação de texto    
 class LimeExplainerSentences:
-    def __init__(self, sigma=25**2, num_samples=15000, K=6, alpha=0.1**(16), p=16,vectorizer=None, model=None, seed=42):
-        self.sigma = sigma
-        self.num_samples = num_samples
-        self.K = K
-        self.alpha = alpha
-        self.p = p
-        self.model = model
-        self.vectorizer = vectorizer
-        np.random.seed(seed)
-        np.set_printoptions(precision=self.p)
+    def __init__(self, sigma=25**2, num_samples=15000, K=6, alpha=0.1**(16), p=16,vectorizer=None, model=None, seed=42, generate="opt"):
+        self.sigma = sigma # Parâmetro do kernel exponencial
+        self.num_samples = num_samples # Número de amostras geradas 
+        self.K = K # Número de palavras importantes
+        self.alpha = alpha # Parâmetro de regularização Lasso
+        self.model = model # Modelo de classificação
+        self.vectorizer = vectorizer # Vetorizador
+        self.generate = generate # Tipo de amostragem
+        self.p = p # Precisão de impressão
+        np.random.seed(seed) # Semente aleatória
+        np.set_printoptions(precision=self.p) # Precisão de impressão
         
 
     # Binarizar vetor de palavras
-    def binarize(self, x):
-        n = len(self.vectorizer.get_feature_names_out())
-        x_bin=np.zeros(n, dtype=int)
-        for i in x.indices:
-            x_bin[i] = 1
-        return x_bin
+    def binarize(self, x): 
+        x = (x.todense() > 0).astype(int) # Binariza o vetor
+        return x.getA1() # Retorna o vetor em formato de array
 
+    def cossine_similarity(self, x, z):
+        x = x.todense() # Transforma o vetor em uma matriz densa
+        z = z.todense()
+        dot_product = x.dot(z.T)
+        norm_x = np.linalg.norm(x)
+        norm_z = np.linalg.norm(z)
+        return (dot_product / (norm_x * norm_z)).getA1()[0]
+    
     # Define a função de kernel 
     def kernel(self, x, z):
-        
-        distance = cosine_similarity(x, z) # Similaridade de cosseno
-        weights = np.sqrt(np.exp(-(distance**2) / (self.sigma**2)))  # Kernel 
-        return weights
-        
-    # Gera dados ao redor de x_line
-    def samples(self, x):
-        
-        n = len(self.vectorizer.get_feature_names_out())
-        x_indices = x.indices
-        n_x_words = len(x.indices)    
-        sample_set = [np.zeros(n) for i in range(self.num_samples-1)]
-        sample_set.append(self.binarize(x))
-        weights = x.tocoo().data
-        weights_normalized = weights / weights.sum()
-        for i in range(self.num_samples-1):
-            num_words = np.random.randint(1, n_x_words+1)
-            z_line_indices = np.random.choice(x_indices, size=num_words, p=weights_normalized, replace=False)
+        distance = self.cossine_similarity(x, z) # Similaridade de cosseno
+        weight = np.sqrt(np.exp(-(distance**2) / (self.sigma**2)))  # Kernel exponencial
+        return weight
+    
+    
+    def samples_simples(self, x):
+        n = len(self.vectorizer.get_feature_names_out()) # Número de palavras no vetorizador
+        x_indices = x.indices # Índices das palavras na sentença
+        n_x_words = len(x.indices) # Número de palavras na sentença   
+        sample_set = [np.zeros(n) for i in range(self.num_samples-1)] # Conjunto de amostras
+        sample_set.append(self.binarize(x)) # Adiciona a sentença original binarizada
+        Z_line_indices = [] # Índices das palavras ativadas
+        for i in range(self.num_samples-1): # Gerar amostras aleatórias
+            num_words = np.random.randint(1, n_x_words+1)  # Número de palavras na amostra
+            # Escolhe aleatoriamente as palavras da sentença original
+            # size = número de palavras na amostra
+            # replace = False, não permite repetição
+            z_line_indices = np.random.choice(x_indices, size=num_words, replace=False)
+            # Ativa as palavras escolhidas
             sample_set[i][z_line_indices] = 1
-        return sample_set
+            Z_line_indices.append(z_line_indices)
+        return sample_set, z_line_indices
+
+    # Gera dados ao redor de x_line relevancia das palavras
+    def samples_opt(self, x):
+        n = len(self.vectorizer.get_feature_names_out()) # Número de palavras no vetorizador
+        x_indices = x.indices # Índices das palavras na sentença
+        n_x_words = len(x.indices) # Número de palavras na sentença   
+        sample_set = [np.zeros(n) for i in range(self.num_samples-1)] # Conjunto de amostras
+        sample_set.append(self.binarize(x)) # Adiciona a sentença original binarizada
+        weights = x.tocoo().data # Pesos das palavras
+        weights_normalized = weights / weights.sum() # Normaliza os pesos
+        Z_line_indices = [] # Índices das palavras ativadas
+        for i in range(self.num_samples-1): # Gerar amostras aleatórias
+            num_words = np.random.randint(1, n_x_words+1)  # Número de palavras na amostra
+            # Escolhe aleatoriamente as palavras da sentença original
+            # size = número de palavras na amostra
+            # p = pesos normalizados, a probabilidade de escolher cada palavra
+            # replace = False, não permite repetição
+            z_line_indices = np.random.choice(x_indices, size=num_words, p=weights_normalized, replace=False)
+            # Ativa as palavras escolhidas
+            sample_set[i][z_line_indices] = 1
+            Z_line_indices.append(z_line_indices)
+        return sample_set, z_line_indices
     
     
     # Transforma um vetor em uma frase
-    def sentences_samples(self, z_line):
-        
-        indices = np.where(z_line == 1)[0]
-        z=" ".join([self.vectorizer.get_feature_names_out()[indice] for indice in indices])
+    def sentences_samples(self, Z_line):
+        for z_line in Z_line:
+            z=" ".join([self.vectorizer.get_feature_names_out()[indice] for indice in Z_line])
         return self.vectorizer.transform([z])
 
     # Define o vetor de pesos
     def LIME(self, x):
-        
-        Z_line = self.samples(x)
+        if self.generate == "opt":
+            Z_line = self.samples_opt
+        else:
+            Z_line = self.samples_simples
         Z=[]
         for i in range(len(Z_line)):
             Z.append(self.sentences_samples(Z_line[i]))
@@ -107,71 +138,10 @@ class LimeExplainerSentences:
 
     # Gerar explicação
     def explain_instance(self, x):
-        
         w = self.LIME(x)
         abs_valores = np.abs(w)
         indices = np.argsort(abs_valores)[::-1][:self.K]
         print("Palavras importantes:")
         for i in indices:
             print(f"{self.vectorizer.get_feature_names_out()[i]}: {w[i]}")    
-
-
-class SubmodularPick:
-    def __init__(self,X_n_vec, X, B,lime=None, vectorizer=None):
-        self.X_n_vec = X_n_vec
-        self.X = X
-        self.B = B
-        self.lime = lime
-        self.vectorizer = vectorizer
-
-    def explain_instances(self, X):
-        W = [self.lime.LIME(x) for x in X] 
-        return np.array(W)      
-        
-    def importancia(self, W):
-        I = np.zeros(W.shape[1])  
-        for j in range(W.shape[1]):
-            soma = np.sum(np.abs(W[:, j]))
-            I[j] = np.sqrt(soma)
-        return I
-
-    def cobertura(self, V, W, I):
-        c_value = 0  
-        for j in range(W.shape[1]):
-            if any(W[i, j] > 0 for i in V):  # Se a característica j é relevante
-                c_value += I[j]
-        return c_value
-
-    def guloso(self, X, B):
-        W = self.explain_instances(X) 
-        I = self.importancia(W)
-        nao_selecionados = list(range(W.shape[0]))  # Lista de índices não selecionados
-        V = []  # Conjunto de características selecionadas
-        itens = 0  # Número de elementos selecionados
-        c_value = 0  # Valor de c
-
-        while itens < B and nao_selecionados:
-            best_gain = -np.inf  # Valor de ganho máximo
-            best_item = None  # Índice do melhor item 
-
-            for item in nao_selecionados:  # Itera sobre os itens não selecionados
-                lista_temp = V + [item]
-                gain = self.cobertura(lista_temp, W, I) - self.cobertura(V, W, I)
-                if gain > best_gain:
-                    best_gain = gain  # Atualiza o valor de ganho máximo
-                    best_item = item  # Atualiza o melhor item
-
-            if best_item is not None:
-                V.append(best_item)  # Adiciona o melhor item ao conjunto
-                nao_selecionados.remove(best_item)  # Remove o melhor item dos não selecionados
-                itens += 1
-                c_value += best_gain
-
-        return V
-    
-    def explain_model(self, X):
-        V = self.guloso(X, self.B)
-        print("Melhor conjunto de explicação: ", V)
-        for i in V:
-            print(f"{self.X_n_vec[i]}")
-        print("Fim da explicação")        
+      
